b79b128aa851a48543c1a4ff1fa7c6d85f654b69
hye0e <nanda5050@naver.com>
2023-05-16 00:46:56 +0900

    FEAT: 저축계좌 API 추가

---
+++ src/main/java/com/boolsazo/bankchall/controller/SavingsAccountController.java
@@ -0,0 +1,64 @@
+package com.boolsazo.bankchall.controller;
+
+import com.boolsazo.bankchall.dto.AccountResponse;
+import com.boolsazo.bankchall.dto.RegistAccountRequest;
+import com.boolsazo.bankchall.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/savings")
+public class SavingsAccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity registerAccount(@RequestBody RegistAccountRequest request) {
+        try {
+            accountService.registSavingAccount(request);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("Withdraw account created successfully.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e);
+        }
+    }
+
+    @GetMapping("/list/{userId}")
+    public ResponseEntity<?> withdrawList(@PathVariable("userId") int userId) {
+        try {
+            AccountResponse response = accountService.withdrawList(userId);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to create withdraw account.");
+        }
+    }
+
+    @DeleteMapping("{accountId}")
+    public ResponseEntity deleteAccount(@PathVariable("accountId") int accountId) {
+        try {
+            accountService.deleteAccount(accountId);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Withdraw account deleted successfully.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/com/boolsazo/bankchall/dto/SavingsRegisterRequest.java
@@ -0,0 +1,27 @@
+package com.boolsazo.bankchall.dto;
+
+import com.boolsazo.bankchall.domain.Account;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavingsRegisterRequest {
+
+    private String accountNumMasked;
+    private String bankName;
+    private boolean isUsed = true;
+    private int userId;
+
+    public Account toEntity() {
+        return new Account(userId,
+                accountNumMasked,
+                bankName,
+                isUsed,
+                1);
+    }
+}
