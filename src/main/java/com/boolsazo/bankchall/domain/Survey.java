102c43fb73df4f4c10eca438af33311dd35094cf
hye0e <nanda5050@naver.com>
2023-05-15 14:03:43 +0900

    FIX: Domain Error

--- src/main/java/com/boolsazo/bankchall/domain/Survey.java
+++ src/main/java/com/boolsazo/bankchall/domain/Survey.java
@@ -1,15 +1,20 @@
 package com.boolsazo.bankchall.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Survey {
-
+    @Id
     @Column(name = "user_id", nullable = false)
     private int userId;
 
