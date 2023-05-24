fe40484da84aa9b1f8781eb1ef0be4b9de0051ec
kimjaeyun <jaeyun1723@naver.com>
2023-05-24 16:56:13 +0900

    Feat : "쥐 이미지 추가" Fix : "필요없는 라이브러리 삭제"

--- frontend/src/pages/IndexSections/Hero.js
+++ frontend/src/pages/IndexSections/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Container, Row, Col} from "reactstrap";
+import {Container, Row, Col} from "reactstrap";
 
 class Hero extends React.Component {
   render() {
