181fbda05e9b202eca1b977d64cbcc779ed654f2
kimjaeyun <jaeyun1723@naver.com>
2023-05-24 13:56:04 +0900

    Feat: Add Main Page image

--- frontend/src/pages/IndexSections/Hero.js
+++ frontend/src/pages/IndexSections/Hero.js
@@ -7,7 +7,13 @@ class Hero extends React.Component {
       <>
         <div className="position-relative">
           {/* Hero for FREE version */}
-          <section className="section section-hero section-shaped">
+          <section className="section section-hero section-shaped"  style={{
+              backgroundImage: `url(${require("../../assets/img/theme/main01.png")})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              width:'1900px',
+              height:'1000px'
+            }}>
             {/* Background circles */}
             <div className="shape shape-style-1 shape-default">
               <span className="span-150" />
@@ -25,60 +31,20 @@ class Hero extends React.Component {
               <div className="col px-0">
                 <Row className="align-items-center justify-content-center">
                   <Col className="text-center" lg="6">
-                    <img
-                      alt="..."
-                      className="img-fluid"
-                      src={require("assets/img/brand/bank_chall_logo.png")}
-                      style={{ width: "200px" }}
-                    />
+                  <div style={{width:'200px'}}></div>
                     <p className="lead text-white">
-                      뱅크 챌린저는 설정한 목표를 달성할 수 있도록 자동이체를 통해 관리해주는 서비스입니다.
                     </p>
                     <div className="btn-wrapper mt-5">
-                      <Button
-                        className="btn-white btn-icon mb-3 mb-sm-0"
-                        color="default"
-                        href="http://localhost:3000/login-page"
-                        size="lg"
-                      >
-                        <span className="btn-inner--icon mr-1">
-                          <i className="ni ni-cloud-download-95" />
-                        </span>
-                        <span className="btn-inner--text" >금융 대사량 테스트</span>
-                      </Button>{" "}
-                      <Button
-                        className="btn-icon mb-3 mb-sm-0"
-                        color="github"
-                        href="https://github.com/boolsazo/bank-challenger"
-                        size="lg"
-                        target="_blank"
-                      >
-                        <span className="btn-inner--icon mr-1">
-                          <i className="fa fa-github" />
-                        </span>
-                        <span className="btn-inner--text">
-                          <span className="text-warning mr-1">불사조</span>
-                          on Github
-                        </span>
-                      </Button>
+                  {" "}
                     </div>
-                    <div className="mt-5">
-                      <small className="text-white font-weight-bold mb-0 mr-2">
-                        producted by
-                      </small>
-                      <img
-                        alt="..."
-                        className="ml-1"
-                        style={{ height: "28px" }}
-                        src={require("assets/img/brand/creativetim-white-slim.png")}
-                      />
-                    </div>
+                   
+                     
                   </Col>
                 </Row>
               </div>
             </Container>
             {/* SVG separator */}
-            <div className="separator separator-bottom separator-skew zindex-100">
+            {/* <div className="separator separator-bottom separator-skew zindex-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 preserveAspectRatio="none"
@@ -92,7 +58,7 @@ class Hero extends React.Component {
                   points="2560 0 2560 100 0 100"
                 />
               </svg>
-            </div>
+            </div> */}
           </section>
         </div>
       </>
