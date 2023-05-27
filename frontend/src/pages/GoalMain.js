768b7e859a24fdecb917a083058bac13d1e4c5d6
yeoni98 <dayeon98@ajou.ac.kr>
2023-05-27 18:46:53 +0900

    create 페이지 연결 관계 수정

--- frontend/src/pages/GoalMain.js
+++ frontend/src/pages/GoalMain.js
@@ -10,273 +10,293 @@ import GoalFirstMain from "./GoalFirstMain";
 import { Button, Card } from "reactstrap";
 import "./CreateGoal.css";
 import RegisterRule from "./RegisterRule";
+import { Link } from 'react-router-dom';
 import {
   Box
 } from "@mui/material";
 
 function GoalMain({ userId }) {
-  const [showCreateGoal, setShowCreateGoal] = useState(false);
-  const [showGoalDetail, setShowGoalDetail] = useState(false);
-  const [showRule, setShowRule] = useState(false);
-  const [selectedGoal, setSelectedGoal] = useState(null);
-  const [goals, setGoals] = useState([]);
-  const [selectedGoalId, setSelectedGoalId] = useState('');
+    const [showCreateGoal, setShowCreateGoal] = useState(false);
+    const [showGoalDetail, setShowGoalDetail] = useState(false);
+    const [showRule, setShowRule] = useState(false);
+    const [selectedGoal, setSelectedGoal] = useState(null);
+    const [goals, setGoals] = useState([]);
+    const [selectedGoalId, setSelectedGoalId] = useState('');
 
-  const updateGoals = () => {
-    axios
-      .get(`/goal/list/${userId}`)
-      .then((res) => {
-        setGoals(res.data.goals);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+    const updateGoals = () => {
+        axios
+            .get(`/goal/list/${userId}`)
+            .then((res) => {
+                setGoals(res.data.goals);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
 
-  useEffect(() => {
-    axios
-      .get(`/goal/list/${userId}`)
-      .then((res) => {
-        setGoals(res.data.goals);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, [userId]);
+    useEffect(() => {
+        axios
+            .get(`/goal/list/${userId}`)
+            .then((res) => {
+                setGoals(res.data.goals);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, [userId]);
 
-  if (goals.length === 0) {
-    return <GoalFirstMain />;
-  }
-  const handleSlideClick = (goalId) => {
-    axios
-      .get(`/goal/detail/${goalId}`)
-      .then((res) => {
-        setSelectedGoal(res.data);
-        setShowGoalDetail(true);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+    const handleSlideClick = (goalId) => {
+        axios
+            .get(`/goal/detail/${goalId}`)
+            .then((res) => {
+                setSelectedGoal(res.data);
+                setShowGoalDetail(true);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
 
-  const handleSlideMouseEnter = (e) => {
-    e.currentTarget.style.backgroundColor = "#172b4d";
-    e.currentTarget.style.color = "#f0f0f0";
-  };
+    const handleSlideMouseEnter = (e) => {
+        e.currentTarget.style.backgroundColor = "#172b4d";
+        e.currentTarget.style.color = "#f0f0f0";
+    };
 
-  const handleSlideMouseLeave = (e) => {
-    e.currentTarget.style.backgroundColor = "#7691F6";
-    e.currentTarget.style.color = "#f0f0f0";
-  };
 
-  const handleCreateGoalClick = () => {
-    setShowCreateGoal(true);
-    updateGoals();
-  };
+    const handleSlideMouseLeave = (e) => {
+        e.currentTarget.style.backgroundColor = "#7691F6";
+        e.currentTarget.style.color = "#f0f0f0";
+    };
 
-  const handleGoalDetailClose = () => {
-    setShowGoalDetail(false);
-    updateGoals();
-  };
+    const handleCreateGoalClick = () => {
+        setShowCreateGoal(true);
 
-  const handleAddRule = (goal) => {
-    //setSelectedGoalId(goalId);
-    setSelectedGoal(goal);
-    setShowRule(true);
-  };
+    };
 
-  const handleRuleClose = () => {
-    setShowRule(false);
-  };
+        // if (goals.length === 0) {
+        //     return (
+        //             <div className="container">
+        //                 <Slider {...settings}>
+        //                 <div
+        //                     color="primary"
+        //                     onClick={handleCreateGoalClick}
+        //                     style={{
+        //                         backgroundColor: "#7691F6",
+        //                         border: "0",
+        //                         width: "450px"
+        //                     }}
+        //                 >
+        //                     목표 생성하기
+        //                     <GoalFirstMain />
+        //                 </div>
+        //                 </Slider>
+        //
+        //     );
+        // }
+        const handleGoalDetailClose = () => {
+            setShowGoalDetail(false);
+            updateGoals();
+        };
 
-  return (
-    <div className="container mukho">
-      <Slider
-        dots={true}
-        infinite={false}
-        speed={500}
-        slidesToShow={3}
-        slidesToScroll={1}
-        centerPadding="20px"
-        arrows={true}
-      >
-        {goals.map((goal) => (
-          <Card className="muk d-flex justify-content-center align-items-center">
-            <div
-              className="slide"
-              onClick={() => handleSlideClick(goal.goalId)}
-              onMouseEnter={handleSlideMouseEnter}
-              onMouseLeave={handleSlideMouseLeave}
-              key={goal.goalId}
-            >
-              <div
-                className="bookmark"
-                style={{ backgroundColor: goal.goalImage }}
-              />
-              <h3>{goal.goalName}</h3>
-              <h2>{goal.goalAmount}</h2>
-              <h2>{goal.startDate}</h2>
+        const handleAddRule = (goal) => {
+            //setSelectedGoalId(goalId);
+            setSelectedGoal(goal);
+            setShowRule(true);
+        };
+
+        const handleRuleClose = () => {
+            setShowRule(false);
+        };
+
+        return (
+            <div className="container mukho">
+                <Slider
+                    dots={true}
+                    infinite={false}
+                    speed={500}
+                    slidesToShow={3}
+                    slidesToScroll={1}
+                    centerPadding="20px"
+                    arrows={true}
+                >
+                    {goals.map((goal) => (
+                        <Card className="muk d-flex justify-content-center align-items-center">
+                            <div
+                                className="slide"
+                                onClick={() => handleSlideClick(goal.goalId)}
+                                onMouseEnter={handleSlideMouseEnter}
+                                onMouseLeave={handleSlideMouseLeave}
+                                key={goal.goalId}
+                            >
+                                <div
+                                    className="bookmark"
+                                    style={{backgroundColor: goal.goalImage}}
+                                />
+                                <h3>{goal.goalName}</h3>
+                                <h2>{goal.goalAmount}</h2>
+                                <h2>{goal.startDate}</h2>
+                            </div>
+                            <Button
+                                className="add-rule"
+                                onClick={() => handleAddRule(goal)}
+                                style={{
+                                    width: "60%",
+                                    justifyContent: "center",
+                                }}
+                            >
+                                규칙 추가
+                            </Button>
+                        </Card>
+                    ))}
+                </Slider>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "50px",
+                    }}
+                >
+                    <Button
+                        color="primary"
+                        onClick={handleCreateGoalClick}
+                        onMouseEnter={handleSlideMouseEnter}
+                        onMouseLeave={handleSlideMouseLeave}
+                        style={{
+                            width: "33%",
+                            backgroundColor: "#7691F6",
+                            border: "0",
+                        }}
+                    >
+                        목표 생성하기
+                    </Button>
+                </div>
+
+                {showCreateGoal && (
+                    <div
+                        style={{
+                            position: "fixed",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                            backgroundColor: "white",
+                            padding: "1em",
+                            zIndex: 1000,
+                            borderRadius: "20px",
+                            boxShadow: "0 2px 30px rgba(0, 0, 0, 0.3)",
+                            width: "450px"
+                        }}
+                    >
+                        <GoalFirstMain/>
+                        <button
+                            onClick={() => setShowCreateGoal(false)}
+                            style={{
+                                fontSize: "12px",
+                                borderRadius: "50px",
+                                color: "black",
+                                position: "fixed",
+                                top: 0,
+                                left: 0,
+                            }}
+                        >
+                            X
+                        </button>
+                    </div>
+                )}
+
+                {showCreateGoal && (
+                    <div
+                        style={{
+                            position: "fixed",
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            backgroundColor: "rgba(0, 0, 0, 0.7)",
+                            zIndex: 999,
+                        }}
+                        onClick={() => setShowCreateGoal(false)}
+                    />
+                )}
+
+                {showGoalDetail && selectedGoal && (
+                    <div className="modal-outer">
+                        <div
+                            className="modal-body"
+                            style={{
+                                position: "fixed",
+                                top: "50%",
+                                left: "50%",
+                                transform: "translate(-50%, -50%)",
+                                backgroundColor: "white",
+                                padding: "1em",
+                                zIndex: 1000,
+                                width: "50%",
+                                height: "75%",
+                            }}
+                        >
+                            <Box sx={{width: "100%", height: "95%"}}>
+                                <GoalDetail
+                                    goal={selectedGoal}
+                                    goalId={selectedGoal.goalId}
+                                    onClose={handleGoalDetailClose}
+                                />
+                            </Box>
+                        </div>
+                    </div>
+                )}
+
+                {showGoalDetail && (
+                    <div
+                        style={{
+                            position: "fixed",
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            backgroundColor: "rgba(0, 0, 0, 0.7)",
+                            zIndex: 999,
+                        }}
+                        onClick={handleGoalDetailClose}
+                    />
+                )}
+
+                {showRule && (
+                    <div
+                        style={{
+                            position: "fixed",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                            backgroundColor: "white",
+                            padding: "1em",
+                            zIndex: 1000,
+                        }}
+                    >
+                        <RegisterRule
+                            goal={selectedGoal}
+                            onClose={handleRuleClose}
+                        />
+                        <button onClick={handleRuleClose}>Close</button>
+                    </div>
+                )}
+
+                {showRule && (
+                    <div
+                        style={{
+                            position: "fixed",
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            backgroundColor: "rgba(0, 0, 0, 0.7)",
+                            zIndex: 999,
+                        }}
+                        onClick={handleRuleClose}
+                    />
+                )}
+
             </div>
-            <Button
-              className="add-rule"
-              onClick={() => handleAddRule(goal)}
-              style={{
-                width: "60%",
-                justifyContent: "center",
-              }}
-            >
-              규칙 추가
-            </Button>
-          </Card>
-        ))}
-      </Slider>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "50px",
-        }}
-      >
-        <Button
-          color="primary"
-          onClick={handleCreateGoalClick}
-          onMouseEnter={handleSlideMouseEnter}
-          onMouseLeave={handleSlideMouseLeave}
-          style={{
-            width: "33%",
-            backgroundColor: "#7691F6",
-            border: "0",
-          }}
-        >
-          목표 생성
-        </Button>
-      </div>
-
-      {showCreateGoal && (
-        <div
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            backgroundColor: "white",
-            padding: "1em",
-            zIndex: 1000,
-            borderRadius: "20px",
-            boxShadow: "0 2px 30px rgba(0, 0, 0, 0.3)",
-          }}
-        >
-          <CreateGoal />
-          <button
-            onClick={() => setShowCreateGoal(false)}
-            style={{
-              fontSize: "12px",
-              borderRadius: "50px",
-              color: "black",
-              position: "fixed",
-              top: 0,
-              left: 0,
-            }}
-          >
-            X
-          </button>
-        </div>
-      )}
-
-      {showCreateGoal && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            zIndex: 999,
-          }}
-          onClick={() => setShowCreateGoal(false)}
-        />
-      )}
-
-      {showGoalDetail && selectedGoal && (
-        <div className="modal-outer">
-        <div
-          className="modal-body"
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            backgroundColor: "white",
-            padding: "1em",
-            zIndex: 1000,
-            width: "50%",
-            height:"75%",
-          }}
-        >
-        <Box sx={{ width: "100%", height:"95%"}}>
-          <GoalDetail
-            goal={selectedGoal}
-            goalId={selectedGoal.goalId}
-            onClose={handleGoalDetailClose}
-          />
-          </Box>
-        </div>
-        </div>
-      )}
-
-      {showGoalDetail && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            zIndex: 999,
-          }}
-          onClick={handleGoalDetailClose}
-        />
-      )}
-
-      {showRule && (
-        <div
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            backgroundColor: "white",
-            padding: "1em",
-            zIndex: 1000,
-          }}
-        >
-          <RegisterRule
-            goal={selectedGoal}
-            onClose={handleRuleClose}
-          />
-          <button onClick={handleRuleClose}>Close</button>
-        </div>
-      )}
-
-      {showRule && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            zIndex: 999,
-          }}
-          onClick={handleRuleClose}
-        />
-      )}
-
-    </div>
-  );
-}
+        );
+    }
 
 export default GoalMain;
