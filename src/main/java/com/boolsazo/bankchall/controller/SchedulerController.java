f2c903363817aff538256da1b599938d5bff0e0f
kimjaeyun <jaeyun1723@naver.com>
2023-05-28 19:37:02 +0900

    Feat.자동이체 api 추가

--- src/main/java/com/boolsazo/bankchall/controller/SchedulerController.java
+++ src/main/java/com/boolsazo/bankchall/controller/SchedulerController.java
@@ -3,6 +3,7 @@ package com.boolsazo.bankchall.controller;
 import com.boolsazo.bankchall.domain.Goal;
 import com.boolsazo.bankchall.domain.SavingHistory;
 import com.boolsazo.bankchall.dto.resultSet.GoalAccountResultSet;
+import com.boolsazo.bankchall.dto.resultSet.SavingHistoryResultSet;
 import com.boolsazo.bankchall.service.GoalAccountService;
 import com.boolsazo.bankchall.service.GoalService;
 import com.boolsazo.bankchall.service.SavingHistoryService;
@@ -34,10 +35,21 @@ public class SchedulerController {
 
         for (Goal goal: goals) {
             try {
-                GoalAccountResultSet  goalSAccountInfo = goalAccountService.showGoalSAccount(goal.getGoalId());
-                SavingHistory vo = new SavingHistory(goalSAccountInfo.getAccount_Id(),goal.getGoalId(),goal.getUserId(),
-                    LocalDateTime.now(),goal.getSavingAmount());
-                savingHistoryService.registerSavingHistory(vo);
+                GoalAccountResultSet goalSAccountInfo = goalAccountService.showGoalSAccount(goal.getGoalId());
+                List<SavingHistoryResultSet> resultSet = savingHistoryService.showSavingHistoryReultSet(goalSAccountInfo.getAccount_Id(),goal.getGoalId());
+                boolean AutomaticDebitPayment=false;
+                for(SavingHistoryResultSet savingDay : resultSet){
+                    if(savingDay.getSaving_Date().getMonth()==LocalDate.now().getMonth()) {
+                        AutomaticDebitPayment = true;
+                        break;
+                    }
+                }
+                if(!AutomaticDebitPayment) {
+                    SavingHistory vo = new SavingHistory(goalSAccountInfo.getAccount_Id(),
+                        goal.getGoalId(), goal.getUserId(),
+                        LocalDateTime.now(), goal.getSavingAmount());
+                    savingHistoryService.registerSavingHistory(vo);
+                }
             } catch (Exception e) {
                 System.out.println("자동 이체: 계좌 정보를 불러오는 과정에서 오류 발생");
             }
